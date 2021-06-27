28fee845834cae4e8aadea838790d0639c0da5df
Jeremy Live_live_live <jeremylive096@gmail.com>
2021-06-26 21:20:39 -0700

    Arreglo, 100% funcional

--- Algoritmos.py
+++ Algoritmos.py
@@ -9,25 +9,15 @@ def FLU():
     contador1 = 0
     contador2 = 0
     contador3 = 0
-    #esta_lleno = False
     
     lista_contadora_roja = []
 
     es_diferente = False
 
-    #Lleno lista contadora roja de ceros
-    #for k in range(0,parametro_largo-1):
-    #    lista_contadora_roja.append(0)
-
-
     #Recorre el arreglo de numeros
     for n in parametros_entrada:
 
-            
-
         #Caso segundo, cuando entra un numero igual
-        #if esta_lleno == True:
-        
         contador1 = 0
         es_diferente = True
 
@@ -36,18 +26,13 @@ def FLU():
 
             #Es el parametro a insertar es igual al dato de la lista
             if n == m:
-                lista_contadora_roja[contador1] = 0
+                lista_contadora_roja[contador1] = 1
                 es_diferente = False
             else:
                 #Se suma uno a los demas numeros
                 lista_contadora_roja[contador1] += 1
 
             contador1 += 1
-            
-        #Caso primero, cuando se llena la memoria
-        if  es_diferente == True and contador2 < parametro_largo:
-            lista_result.append(n)
-            lista_contadora_roja.append(1)
             
         #Caso tercero, cuando entra un numero diferente
         if es_diferente == True and contador2 >= parametro_largo:
@@ -68,24 +53,20 @@ def FLU():
 
             #Sustituyo el numero que a estado mas tiempo
             lista_result[indice_encontrado] = n   
-            lista_contadora_roja[indice_encontrado] = 0 
+            lista_contadora_roja[indice_encontrado] = 1
 
-        contador2 += 1
-
-
-        #Se lleno la lista de resultado
-        #if len(lista_result) == parametro_largo-1:
-        #    esta_lleno = True
-
-
+        #Caso primero, cuando se llena la memoria
+        if  es_diferente == True and contador2 < parametro_largo:
+            lista_result.append(n)
+            lista_contadora_roja.append(1)
+            contador2 += 1
+            
         #Imprimo corrida
         print("\nLista de resultado: ")
         print(lista_result)
 
         print("Lista de contadores: ")
         print(lista_contadora_roja)
-
-
 
 #Compilo
 FLU()
